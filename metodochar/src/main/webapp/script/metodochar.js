437fc98f481ec98ead2450843071f3b99e393de3
DESKTOP-5AFE429\Isidro Camacho <isidroswicth@gmail.com>
2023-01-30 08:57:15 +0100

    30-1-2023

---
+++ metodochar/src/main/webapp/script/metodochar.js
@@ -0,0 +1,7 @@
+function eje1(){
+let string ="hola buenas tardes como estas todo bien?";
+document.write(string.charAt(0)+"<br>");
+document.write(string.charAt(1)+"<br>");
+document.write(string.charAt(2)+"<br>");
+document.write(string.charAt(3)+"<br>");
+}
